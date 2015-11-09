9557051d3891ed30497296457b05d9b0a9b6864c
Wesley Harris <wesley@wharris.com>
2015-11-09 11:40:45 -0500

    Add ansible galaxy roles

--- bootstrap-control-machine.sh
+++ bootstrap-control-machine.sh
@@ -3,6 +3,28 @@
 # Usage: sudo ./boostrap-control-machine.sh
 
 # OSX
-easy_install pip
-pip install ansible
+# Install pip and ansible
+
+is_installed () {
+  hash $1 2>/dev/null
+}
+
+install_pip () {
+  easy_install pip
+}
+
+install_ansible () {
+  pip install ansible
+}
+
+if ! is_installed pip; then
+  install_pip
+fi
+
+if ! is_installed ansible; then
+  install_ansible
+fi
+
+# Install galaxy roles
+ansible-galaxy install --force -r roles.txt
 
